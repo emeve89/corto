f64da6edae3bcca7c5e637c12a4203439be88477
Mario Esteban Viapiano <marioeviapiano@gmail.com>
2019-02-21 21:10:11 +0100

    Refactor Bijective module

--- lib/corto/use_cases/url/utils/bijective.rb
+++ lib/corto/use_cases/url/utils/bijective.rb
@@ -3,25 +3,24 @@ module Corto
     module Url
       module Utils
         module Bijective
-          ALPHABET = (('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a).join
+          ALPHABET = (('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a).join.freeze
+          BASE = ALPHABET.size.freeze
 
           private
 
           def encode(id)
             return ALPHABET[0] if id.zero?
-            s = ''
-            base = ALPHABET.length
-            while id > 0
-              s << ALPHABET[id.modulo(base)]
-              id /= base
-            end
-            s.reverse
+            ''.tap do |s|
+              while id > 0
+                s << ALPHABET[id.modulo(BASE)]
+                id /= BASE
+              end
+            end.reverse
           end
 
           def decode(base64)
             i = 0
-            base = ALPHABET.length
-            base64.each_char { |c| i = i * base + ALPHABET.index(c) }
+            base64.each_char { |char| i = i * BASE + ALPHABET.index(char) }
             i
           end
         end
